23b5cf11ddc975a6a851720678a53c1a11a3a376
chenja99 <chenja99@yahoo.com>
2021-03-23 20:44:50 -0400

    second commit, edit some text

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>PHP Store</title>
+    <title> PHP Test </title>
   </head>
   <body>
     <?php
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
-    echo "<h1>Welcome!</h1>";
+    echo "<h1>Welcome Back!</h1>";
     ?>
   </body>
 </html>
